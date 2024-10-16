f935005119b44f0d2b06af3b317c54d694288e2d
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2024-10-16 21:35:18 +0530

    Time: 2 ms (52.46%), Space: 41 MB (32.42%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -10,33 +10,35 @@ class Solution {
     public String longestDiverseString(int a, int b, int c) {
         PriorityQueue<Pair> q = new PriorityQueue<>((o1, o2) -> o2.c - o1.c);
 
-        q.add(new Pair('a', a));
-        q.add(new Pair('b', b));
-        q.add(new Pair('c', c));
+        if(a> 0) q.add(new Pair('a', a));
+        if(b>0) q.add(new Pair('b', b));
+        if(c > 0) q.add(new Pair('c', c));
 
-        String s = "";
+        StringBuilder s = new StringBuilder();
         while (s.length() <= (a+b+c)){
+            if(q.isEmpty()) break;
             Pair curr = q.poll();
             if(curr.c == 0) break;
             if(s.length() < 2){
-                s += curr.ch;
+                s.append(curr.ch);
                 q.offer(new Pair(curr.ch, curr.c - 1));
             }
             else if(s.charAt(s.length()-1) == curr.ch && s.charAt(s.length() - 2) == curr.ch){
+                if(q.isEmpty()) break;
                 Pair s_curr = q.poll();
                 if(s_curr.c == 0) break;
 
-                s += s_curr.ch;
+                s.append(s_curr.ch);
                 q.offer(new Pair(s_curr.ch, s_curr.c - 1));
                 q.offer(curr);
             }
             else{
-                s += curr.ch;
+                s.append(curr.ch);
                 q.offer(new Pair(curr.ch, curr.c -1));
             }
         }
 
-        return s;
+        return s.toString();
 
     }
 }
