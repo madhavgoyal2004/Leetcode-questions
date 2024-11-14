823bbb0a5b106b0517f37e730420ff14038de24d
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2024-11-14 22:20:00 +0530

    Time: 115 ms (26.07%), Space: 58.4 MB (6.99%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canDistribute(int [] quantities, int mid, int n){
+        int sum = 0;
+        for(int i : quantities){
+            if(i % mid == 0) sum += i/(double)mid;
+            else sum += (i/(double)mid + 1);
+        }
+
+        return sum <= n;
+    }
+    public int minimizedMaximum(int n, int[] quantities) {
+        int st = 1;
+        int end = Integer.MIN_VALUE;
+        int mid = 0;
+
+        System.out.println(Arrays.toString(quantities));
+        for(int i : quantities){
+            end = Math.max(end , i);
+        }
+        if(n == quantities.length) return end;
+        int ans = 0;
+        while (st <= end){
+            mid = st + (end - st)/2;
+
+            if(canDistribute(quantities, mid, n)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        return ans ;
+    }
+}
