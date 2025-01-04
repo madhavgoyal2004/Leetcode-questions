a35ad0e30a9d0541540df4407e76b1861d27b2a2
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-01-04 14:46:04 +0530

    Time: 170 ms (72.77%), Space: 45.4 MB (71.2%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,22 +1,22 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        HashMap<Character, List<Integer>> map = new HashMap<>();
+        HashMap<Character, int[]> map = new HashMap<>();
         for(int i=0; i<s.length() ; i++){
             char curr = s.charAt(i);
             if(map.containsKey(curr)){
-                map.get(curr).add(i);
+                map.get(curr)[1] = i;
             }
             else{
-                List<Integer> list = new ArrayList<>();
-                list.add(i);
-                map.put(curr , list);
+                int [] arr = new int[2];
+                arr[0] = i;
+                map.put(curr , arr);
             }
         }
 
         int count = 0;
         for(Character c : map.keySet()){
-            int start = map.get(c).get(0);
-            int end = map.get(c).get(map.get(c).size() - 1);
+            int start = map.get(c)[0];
+            int end = map.get(c)[1];
             HashSet<Character> set = new HashSet<>();
             for(int i=start + 1; i < end; i++){
                 set.add(s.charAt(i));
