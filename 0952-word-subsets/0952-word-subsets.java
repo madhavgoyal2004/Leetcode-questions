ac2bead193a18c6b91e9ab046b9a12ec442b54af
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-01-10 17:32:02 +0530

    Time: 11 ms (100%), Space: 53.8 MB (91.28%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -2,19 +2,14 @@ class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         List<String> list = new ArrayList<>();
         int [] arr = new int[26];
-        for(char ch = 'a' ; ch<='z'; ch++){
-            for(String a : words2){
-                int count = 0;
-                for(int i=0; i<a.length(); i++){
-                    char c = a.charAt(i);
-                    if(ch == c){
-                        count++;
-                        arr[ch-'a'] = Math.max(arr[ch-'a'], count);
-                    }
-                }
+        for(String a : words2){
+            int [] temp = new int[26];
+            for(int i=0; i<a.length(); i++){
+                char c = a.charAt(i);
+                temp[c-'a']++;
+                arr[c-'a'] = Math.max(arr[c-'a'], temp[c-'a']);    
             }
         }
-        // System.out.println(Arrays.toString(arr));
 
         for(String b : words1){
             int [] temp = new int[26];
@@ -22,7 +17,6 @@ class Solution {
                 char c = b.charAt(i);
                 temp[c-'a']++;
             }
-            // System.out.println(Arrays.toString(temp));
             boolean f = true;
             for(int i=0; i<26; i++){
                 if(arr[i] > 0 && temp[i] < arr[i]){
