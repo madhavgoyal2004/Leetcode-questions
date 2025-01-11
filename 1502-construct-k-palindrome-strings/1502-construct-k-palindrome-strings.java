1705b2874f08357808e27d07e7ce7af9351555d8
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-01-11 09:03:43 +0530

    Time: 5 ms (66.55%), Space: 45.4 MB (61.15%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        // int [] arr = new int[26];
-        // for(int i=0; i<s.length(); i++){
-        //     char c = s.charAt(i);
-        //     arr[c-'a']++;
-        // }
         if(s.length() < k) return false;
         if(s.length() == k) return true;
-
-        HashMap<Character, Integer> map = new HashMap<>();
+        int [] arr = new int[26];
         for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+            char c = s.charAt(i);
+            arr[c-'a']++;
         }
         // System.out.println(map);
         int count = 0;
-        for(Character c : map.keySet()){
-            if(map.get(c) % 2 == 1){
+        for(int i=0; i<26; i++){
+            if(arr[i] % 2 == 1){
                 count++;
             }
         }
