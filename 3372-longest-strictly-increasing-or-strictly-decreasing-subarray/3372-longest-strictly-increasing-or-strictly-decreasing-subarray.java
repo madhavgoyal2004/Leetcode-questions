eb67bfb849419e9156b388190bd6ed8327934db8
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-02-03 09:48:29 +0530

    Time: 1 ms (93.94%), Space: 43.2 MB (15.49%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -4,14 +4,14 @@ class Solution {
         if(n==1)return 1;
         int a = 1;
         int b = 1;
-        int max = 0;
+        int max = 1;
 
-        for(int i=1; i<n; i++){
-            if(nums[i-1] > nums[i]){
+        for(int i=0; i<n-1; i++){
+            if(nums[i] > nums[i+1]){
                 b=1;
                 a++;
             }
-            else if(nums[i-1] < nums[i]){
+            else if(nums[i] < nums[i+1]){
                 a=1;
                 b++;
             }
