de5a098d8ab6d9affcbadfac305eaede13bb8de5
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-02-06 21:04:01 +0530

    Time: 202 ms (69.23%), Space: 74.7 MB (42.95%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -2,24 +2,17 @@ class Solution {
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
         if(n < 4) return 0;
-        HashMap<Integer, ArrayList<int[]>> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
-                int sum = nums[i]*nums[j];
-                if(map.containsKey(sum)){
-                    map.get(sum).add(new int [] {i,j});
-                }
-                else{
-                    ArrayList<int []> arr = new ArrayList<>();
-                    arr.add(new int [] {i, j});
-                    map.put(sum, arr);
-                }
+                int sum = nums[i] * nums[j];
+                map.put(sum, map.getOrDefault(sum, 0)+1);
             }
         }
 
         for(int i : map.keySet()){
-            int size = map.get(i).size();
+            int size = map.get(i);
             if(size >= 2){
                 count += (size*(size-1))/2; 
             }
