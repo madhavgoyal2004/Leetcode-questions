d15ec9e70417ef0cf2eba33a925d38bdefface73
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-03-12 15:41:33 +0530

    Time: 0 ms (100%), Space: 44.9 MB (63.3%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,15 +1,40 @@
 class Solution {
+    private int helper (int [] nums, int st, int end){
+        while(st < end){
+            int mid = st + (end - st)/2;
+
+            if(nums[mid] < 0){
+                st = mid + 1;
+            }
+            else if(nums[mid] > 0){
+                end = mid - 1;
+            }
+            else return mid;
+        }
+
+        return st;
+    }
     public int maximumCount(int[] nums) {
-        int neg = 0;
-        int pos = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] < 0){
-                neg++;
-            } else if(nums[i] > 0){
-                pos++;
+        int n = nums.length;
+        int neg = -1;
+        int pos = n;
+        
+        int idx = helper(nums, 0, n-1);
+
+    
+        for(int i=idx ; i>=0; i--){
+            if(nums[i] < 0) {
+                neg = i;
+                break;
+            }
+        }
+        for(int i=idx; i<n; i++){
+            if(nums[i] > 0){
+                pos = i;
+                break;
             }
         }
 
-        return Math.max(neg, pos);
+        return Math.max(neg+1, n-pos);
     }
 }
