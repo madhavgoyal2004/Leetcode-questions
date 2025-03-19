8231eb940ef9f8f7278b4b947989fcf075b9c240
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-03-19 08:45:45 +0530

    Time: 1 ms (99.4%), Space: 42.1 MB (18.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,11 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         int xor = 0;
-        for(int i=0; i<s.length(); i++){
-            xor ^= s.charAt(i);
+        for(char c : s.toCharArray()){
+            xor ^= c;
         }
-        for(int i=0; i<t.length(); i++){
-            xor ^= t.charAt(i);
+        for(char c : t.toCharArray()){
+            xor ^= c;
         }
         return (char)xor;
     }
