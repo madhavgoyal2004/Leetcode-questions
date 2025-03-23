c413d392f7ebb8144da305af9f2a0540bdb6aae4
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-03-24 05:06:59 +0530

    Time: 3 ms (23.64%), Space: 58.1 MB (91.9%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -10,49 +10,34 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        // ListNode prevstart = null;
         ListNode kstart = null;
-        // ListNode nextstart = null;
-        // ListNode prevend = null;
         ListNode kend = null;
-        // ListNode nextend = null;
         int size = 0;
-        ListNode c = head;
-
-        while(c != null){
-            size++;
-            c = c.next;
-        }
-        System.out.println(size);
-
-        int temp = k;
-        int tem = 0;
         ListNode curr = head;
 
         while(curr != null){
+            size++;
+            curr = curr.next;
+        }
+
+        int temp = k;
+        int tem = 0;
+        curr = head;
+
+        while(curr != null){
             if(temp-- > 0){
-                // prevstart = curr;
                 kstart = curr;
             }
             if(tem <= size - k){
-                // prevend = curr;
                 kend = curr;
                 tem++;
             }
             curr = curr.next;
         }
 
-        // System.out.println(prevstart.val + " " + prevend.val);
-        // kstart = prevstart.next;
-        // System.out.println(kstart.val);
-        // if(prevend.next != null) kend = prevend.next;
-        // if(kend.next != null) nextend = kend.next;
-        
-        // if(kstart.next != null)nextstart = kstart.next;
-
-        int t = kstart.val ;
+        temp = kstart.val ;
         kstart.val = kend.val;
-        kend.val = t; 
+        kend.val = temp; 
 
         return head;
     }
