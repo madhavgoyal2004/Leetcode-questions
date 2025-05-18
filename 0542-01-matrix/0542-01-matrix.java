b31baea82d3be94e8d16056b3204d1db38d0ba2c
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-05-18 16:44:51 +0530

    Time: 15 ms (38.76%), Space: 47.4 MB (56.68%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,25 +1,14 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        int val;
-        Pair(int row , int col, int val){
-            this.row = row;
-            this.col = col;
-            this.val = val;
-        }
-    }
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int [] r = {-1,0,1,0};
         int [] c = {0,-1,0,1};
-        Queue<Pair> q = new LinkedList<>();
-        boolean [][] visit = new boolean[m][n];
+        Queue<int []> q = new LinkedList<>();
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0) {
-                    q.add(new Pair (i, j, 0));
+                    q.add(new int [] {i,j});
                 }
                 else{
                     mat[i][j] = Integer.MAX_VALUE;
@@ -28,19 +17,18 @@ class Solution {
         }
 
         while(!q.isEmpty()){
-            Pair curr = q.poll();
-            int x = curr.row;
-            int y = curr.col;
-            if(visit[x][y]) continue;
-
-            visit[x][y] = true;
+            int [] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
 
             for(int i=0; i<4; i++){
                 int nr = x + r[i];
                 int nc = y + c[i];
                 if(nr >=0 && nc >=0 && nr < m && nc < n && mat[nr][nc] != 0){
-                    mat[nr][nc] = Math.min(mat[nr][nc], curr.val + 1);
-                    q.add(new Pair(nr, nc, mat[nr][nc]));
+                    if(mat[nr][nc] > mat[x][y] + 1){
+                        mat[nr][nc] = mat[x][y] + 1;
+                        q.add(new int [] {nr, nc});
+                    }
                 }
             }
         }
