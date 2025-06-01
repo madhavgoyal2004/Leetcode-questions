ea032b90cd0ec00fc04d328bc47f2e3396ee1285
Madhav Goyal <111670843+madhavgoyal2004@users.noreply.github.com>
2025-06-01 10:29:25 +0530

    Time: 3 ms (99.93%), Space: 44.9 MB (89.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,43 @@
 class Solution {
+    public boolean cycle(ArrayList<ArrayList<Integer>> graph, boolean[] visited, boolean[] path_visited, int src){
+        visited[src] = true;
+        path_visited[src] = true;
+
+        for(int nbr :  graph.get(src)){
+            if(!visited[nbr]){
+                if(cycle(graph, visited, path_visited, nbr)) return true;
+            }
+            else if(visited[nbr] && path_visited[nbr]) return true;
+        }
+
+        path_visited[src] = false;
+
+        return false;
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean [] visited = new boolean[numCourses];
+        boolean [] path_visited = new boolean[numCourses];
+        
+
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<numCourses; i++){
+
+        for(int i=0; i<numCourses;i++){
             graph.add(new ArrayList<>());
         }
 
         for(int [] arr : prerequisites){
-            graph.get(arr[0]).add(arr[1]);
+            int a = arr[0];
+            int b = arr[1];
+            graph.get(b).add(a);
         }
+        // System.out.println(graph);
 
-        Stack<Integer> st = new Stack<>();
-        boolean [] visited = new boolean[numCourses];
         for(int i=0; i<numCourses; i++){
-            if(!visited[i])
-            topoSort(graph, numCourses, visited, i, st);
+            if(!visited[i]){
+                if(cycle(graph, visited, path_visited, i)) return false;
+            }
         }
-        System.out.println(st);
 
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        while(!st.isEmpty()){
-            arr.add(st.pop());
-        }
-        System.out.println(arr);
-
-        for(int[] edge : prerequisites){
-            int a = arr.indexOf(edge[0]);
-            int b = arr.indexOf(edge[1]);
-
-            if(a >= b) return false;
-        }
         return true;
-    }
-
-    public void topoSort(ArrayList<ArrayList<Integer>> graph, int V, boolean [] visited, int src, Stack<Integer> st){
-        if(visited[src]) return;
-        visited[src] = true;
-        for(int nbr : graph.get(src)){
-            topoSort(graph, V, visited, nbr ,  st);
-        }
-        st.push(src);
     }
 }
